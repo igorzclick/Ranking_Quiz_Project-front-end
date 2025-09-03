afe4335516ac43e575b3b224ff75ddebe494feeb
Matheus Abreu <matheusabreutech@gmail.com>
2025-09-03 20:19:59 -0300

    fix: fixing provider bug first steps project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import './App.css'
-import { Avatar, Button, Card } from "@chakra-ui/react"
-
-
+import './App.css';
+import { Avatar, Button, Card } from '@chakra-ui/react';
+import { Provider } from './components/ui/provider';
 
 function App() {
-
-
   return (
-    <Card.Root width="320px">
-      <Card.Body gap="2">
-        <Avatar.Root size="lg" shape="rounded">
-          <Avatar.Image src="https://picsum.photos/200/300" />
-          <Avatar.Fallback name="Nue Camp" />
-        </Avatar.Root>
-        <Card.Title mt="2">Nue Camp</Card.Title>
-        <Card.Description>
-          This is the card body. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-          Curabitur nec odio vel dui euismod fermentum.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer justifyContent="flex-end">
-        <Button variant="outline">View</Button>
-        <Button>Join</Button>
-      </Card.Footer>
-    </Card.Root>
-
-    
-  )
+    <Provider>
+      <Card.Root width='320px'>
+        <Card.Body gap='2'>
+          <Avatar.Root size='lg' shape='rounded'>
+            <Avatar.Image src='https://picsum.photos/200/300' />
+            <Avatar.Fallback name='Nue Camp' />
+          </Avatar.Root>
+          <Card.Title mt='2'>Nue Camp</Card.Title>
+          <Card.Description>
+            This is the card body. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
+            Curabitur nec odio vel dui euismod fermentum.
+          </Card.Description>
+        </Card.Body>
+        <Card.Footer justifyContent='flex-end'>
+          <Button variant='outline'>View</Button>
+          <Button>Join</Button>
+        </Card.Footer>
+      </Card.Root>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
